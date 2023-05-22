cc3be3b8971cfef9497ec48003636a2b5e91136c
Alexandre-Gb <giboz.alexandre@gmail.com>
2023-05-22 20:43:00 +0200

    Compact constructor

--- TP 12 Révisions/src/fr/umlv/monopoly/Hotel.java
+++ TP 12 Révisions/src/fr/umlv/monopoly/Hotel.java
@@ -1,16 +1,14 @@
 package fr.umlv.monopoly;
 
 public record Hotel (int rooms, double efficiency) implements Asset {
-	
-	public Hotel(int rooms, double efficiency) {
+
+	public Hotel {
 		if (rooms < 0) {
 			throw new IllegalArgumentException("Negative rooms not allowed");
 		}
 		if (efficiency < 0 || efficiency > 1) {
 			throw new IllegalArgumentException("Efficiency value must be between 0 and 1");
 		}
-		this.efficiency = efficiency;
-		this.rooms = rooms;
 	}
 	
 	@Override
